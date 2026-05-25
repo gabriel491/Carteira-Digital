d8c7295956bc4a343a070c08e46fc165ec99c32c
Gabriel Macedo <gmreal570@gmail.com>
2026-05-25 08:30:15 -0300

    Configura o interceptor que  adiciona o token de autenticação em todas as requisições

--- app_finance_front/src/services/api.js
+++ app_finance_front/src/services/api.js
@@ -4,4 +4,13 @@ const api = axios.create({
   baseURL: "http://localhost:3000", 
 });
 
+// Interceptador para adicionar o token de autenticação em todas as requisições
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("@Finance:token")
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+})
+
 export default api;
